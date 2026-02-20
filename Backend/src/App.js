837e39df3a25ac3728211403464f29d921360fa2
ronak bendwal <ronakbendwal7880@gmail.com>
2026-02-20 23:35:47 +0530

    Configure All The Middleware For Letter Use

--- Backend/src/App.js
+++ Backend/src/App.js
@@ -1,9 +1,26 @@
-import express from 'express';
+import express, { urlencoded,static as static_,json } from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+import { ErrorMiddleware } from './Middleware/index.js';
 
 const app=express();
 
-app.get('/',(req,res)=>{
-  res.send("server");
-})
+app.use(cookieParser());
+
+app.use(static_("public"));
+
+app.use(urlencoded({extended:true, limit:"16kb"}));
+
+app.use(json({limit:"50kb"}));
+
+app.use(
+  cors({
+    origin:"https://localhost:5173",
+    Credential:true
+  })
+)
+
+
+app.use(ErrorMiddleware);
 
 export default app;
